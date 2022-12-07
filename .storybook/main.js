7609935a3e12a510f4fdec23054e0251db456d65
blistok <slobodan.dabovic@codeandtheory.com>
2022-12-07 13:25:04 -0700

    Add Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+module.exports = {
+  stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "addon-screen-reader",
+    "storybook-addon-designs",
+    "@degjs/storybook-addon-taffy/register",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
