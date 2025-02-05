cae67dd4b1777a2f2d96410143975a6ab2f26926
vineethct <vineeth.vpai@codeandtheory.com>
2025-02-05 17:34:04 +0530

    loading broswe

--- src/app/(home)/browse/loading.tsx
+++ src/app/(home)/browse/loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <div className="p-5">
+      <div className="h-16 w-1/6 animate-pulse rounded bg-gray-300"></div>
+      <div className="mt-3 h-8 w-1/5 animate-pulse rounded bg-gray-300"></div>
+    </div>
+  );
 };
 
 export default loading;
