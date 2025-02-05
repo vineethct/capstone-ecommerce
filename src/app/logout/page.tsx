f13926d87c299e77825621c55cf3b36feefb83cc
vineethct <vineeth.vpai@codeandtheory.com>
2025-02-05 13:35:55 +0530

    clear redux cart on logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -3,14 +3,17 @@ import { useUserCookieStore } from "@/store/user-cookie-store";
 import { useRouter } from "next/navigation";
 import { PAGE_ROUTES } from "@/lib/constants";
 import { useEffect } from "react";
+import { useCartStore } from "@/store/cart-store";
 
 const Logout = () => {
   const router = useRouter();
   const { setUserToken, setDecoded } = useUserCookieStore();
+  const { clearItems } = useCartStore();
 
   useEffect(() => {
     setUserToken("");
     setDecoded(undefined);
+    clearItems();
     router.replace(PAGE_ROUTES.LOGIN);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
